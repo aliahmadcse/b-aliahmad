0e5a66e6cb590652b34d16c6b399cd01af73860f
aliahmadcse <aliahmadcse@gmail.com>
2020-07-14 23:02:57 +0500

    Adds google analytics

--- storage/framework/views/e2d0390fbdcceb6ad65bc87804f38576afd40ce9.php
+++ storage/framework/views/e2d0390fbdcceb6ad65bc87804f38576afd40ce9.php
@@ -2,6 +2,17 @@
 <html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
 
     <head>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141108427-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-141108427-2');
+        </script>
+
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
